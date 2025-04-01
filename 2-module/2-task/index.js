4c7b53d4ce3ec83309f839e5b1ac136c3dbcd085
hasky24 <hasky24@gmail.com>
2025-04-01 19:04:48 +0300

    created function isEmpty

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,3 +1,20 @@
 function isEmpty(obj) {
-  // ваш код...
+  let count = true;
+
+  for (let key in obj) {
+    count = false;
+  }
+
+  return count;
 }
+
+/*
+Альтернативное решение задачи с помощью метода Object.keys 
+(читала эту главу ранее при самостоятельном изучении учебника)
+
+function isEmpty(obj) {
+  if (Object.keys(obj).length == 0) return true;
+  
+  return false;
+}
+*/
