42493cb68b25d99d1a6199dd685ba3d3d458c11f
hasky24 <hasky24@gmail.com>
2025-04-05 18:34:41 +0300

    fixed: поменяла возвращаемые значения объекта со строк на числа

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -3,7 +3,8 @@ function getMinMax(str) {
 
   let arr = str.split(' ')
     .filter(item => isFinite(item))
-    .sort((a, b) => a - b);
+    .sort((a, b) => a - b)
+    .map(item => +item)
 
   obj.min = arr[0];
   obj.max = arr.at(-1);
