83bb741e82868e50d88e3c74686810243feb16fe
hasky24 <hasky24@gmail.com>
2025-04-09 18:31:14 +0300

    create function makeFriendsList

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,23 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let ul = document.createElement('ul');
+
+  for (let friend of friends) {
+    let li = document.createElement('li');
+    li.innerHTML = `${friend.firstName} ${friend.lastName}`;
+    ul.append(li);
+  }
+
+  return ul;
 }
+
+/* Второй вариант
+
+function makeFriendsList(friends) {
+  let ul = document.createElement('ul');
+
+  for (let friend of friends) {
+    ul.innerHTML += `<li>${friend.firstName} ${friend.lastName}</li>`;
+  }
+  return ul;
+}
+*/
