c768cfe4603af0bc4d3409e38e2451e732b74e88
hasky24 <hasky24@gmail.com>
2025-04-18 19:11:01 +0300

    create class UserTable

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -13,6 +13,59 @@
  *
  */
 export default class UserTable {
+  #rows = [];
+  elem = null;
+
   constructor(rows) {
+    this.#rows = rows || this.#rows;
+    this.#render();
+  }
+
+  #template() {
+    return `
+    <table>
+      <thead>
+          <tr>
+              <th>Имя</th>
+              <th>Возраст</th>
+              <th>Зарплата</th>
+              <th>Город</th>
+              <th></th>
+          </tr>
+      </thead>
+      <tbody>
+        ${this.#rows.map((row) => ` <tr>
+            <td>${row.name}</td>
+            <td>${row.age}</td>
+            <td>${row.salary}</td>
+            <td>${row.city}</td>
+            <td><button class='delete_button'>X</button></td>
+            </tr>`)
+        .join('')}
+      </tbody>
+    </table>
+  `;
+  }
+
+  #createElement(html) {
+    const div = document.createElement('div');
+    div.innerHTML = html;
+    return div.firstElementChild;
+  }
+
+  #deleteRow = (deleteButton) => {
+    deleteButton.closest('tr').remove()
+  }
+
+  #render() {
+    this.elem = this.#createElement(this.#template());
+
+    this.elem.addEventListener('click', (event) => {
+      const deleteButton = event.target.closest('.delete_button');
+
+      if (deleteButton) {
+        this.#deleteRow(deleteButton)
+      }
+    })
   }
 }
