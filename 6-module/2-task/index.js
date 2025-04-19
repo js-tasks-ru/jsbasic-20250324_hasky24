b05e108f26b6801b3dffff1a0369947071e346dc
hasky24 <hasky24@gmail.com>
2025-04-19 12:03:46 +0300

    create class ProductCard

--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,4 +1,44 @@
+import createElement from '../../assets/lib/create-element.js';
+
 export default class ProductCard {
+
+  elem = null;
+  #product = {};
+
   constructor(product) {
+    this.#product = product || this.#product;
+    this.#render();
+  }
+
+  #createTemplate() {
+    return `
+    <div class="card">
+      <div class="card__top">
+        <img src="/assets/images/products/${this.#product.image}" class="card__image" alt="product">
+        <span class="card__price">€${this.#product.price.toFixed(2)}</span>
+      </div>
+      <div class="card__body">
+        <div class="card__title">${this.#product.name}</div>
+        <button type="button" class="card__button">
+          <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+        </button>
+      </div>
+    </div>
+    `;
+  }
+
+  #addProduct = () => {
+    const event = new CustomEvent('product-add', {
+      detail: this.#product.id,
+      bubbles: true
+    });
+
+    this.elem.dispatchEvent(event);
+  }
+
+  #render() {
+    this.elem = createElement(this.#createTemplate());
+
+    this.elem.addEventListener('click', this.#addProduct);
   }
 }
