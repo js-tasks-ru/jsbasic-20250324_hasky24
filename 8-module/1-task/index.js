8de4ad6f7076056936daf70721d42b5c7287d4e8
hasky24 <hasky24@gmail.com>
2025-04-27 21:08:43 +0300

    create function updatePosition

--- 8-module/1-task/index.js
+++ 8-module/1-task/index.js
@@ -1,6 +1,8 @@
 import createElement from '../../assets/lib/create-element.js';
 
 export default class CartIcon {
+  topCard = null;
+
   constructor() {
     this.render();
 
@@ -21,12 +23,14 @@ export default class CartIcon {
           <span class="cart-icon__price">€${cart.getTotalPrice().toFixed(2)}</span>
         </div>`;
 
+      this.topCard = this.elem.getBoundingClientRect().top + window.pageYOffset;
+
       this.updatePosition();
 
       this.elem.classList.add('shake');
       this.elem.addEventListener('transitionend', () => {
         this.elem.classList.remove('shake');
-      }, {once: true});
+      }, { once: true });
 
     } else {
       this.elem.classList.remove('cart-icon_visible');
@@ -39,6 +43,35 @@ export default class CartIcon {
   }
 
   updatePosition() {
-    // ваш код ...
+
+    if (this.elem.classList.contains('cart-icon_visible')) {
+
+      let leftIndent = Math.min(
+        document.querySelector('.container').getBoundingClientRect().right + 20,
+        document.documentElement.clientWidth - this.elem.offsetWidth - 10
+      ) + 'px';
+
+      if (window.pageYOffset > this.topCard) {
+
+        Object.assign(this.elem.style, {
+          position: 'fixed',
+          top: '50px',
+          zIndex: 1e3,
+          right: '10px',
+          left: leftIndent
+        });
+      }
+
+      if (window.pageYOffset < this.topCard || document.documentElement.clientWidth <= 767) {
+
+        Object.assign(this.elem.style, {
+          position: '',
+          top: '',
+          left: '',
+          right: '',
+          zIndex: ''
+        });
+      }
+    }
   }
 }
